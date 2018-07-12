fba0ad5159264ebe341b9ef03ed107bf08fe974e
Seth B <sethbaah1@yahoo.com>
2018-07-12 10:07:39 -0400

    tester

--- tester.py
+++ tester.py
@@ -3,5 +3,4 @@ from client import PokemonApiClient
 client = PokemonApiClient()
 p = PokemonApiClient().getItem(1)
 
-print(p.name + "\n" + p.category + "\n" + p.attributes + "\n" + p.shortEffect + "\n" + p.cost)
 
